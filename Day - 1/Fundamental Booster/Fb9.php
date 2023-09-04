da679d1042c5b3703f9c6120337ec6893844b409
Deep Patel <118447327+deepbajud@users.noreply.github.com>
2023-09-04 15:47:43 +0530

    Create Fb9.php
    Write a PHP Program to convert Km to Mile (find formula and calculate) 10. Write a PHP Program to swap two numbers using third variable.

---
+++ Day - 1/Fundamental Booster/Fb9.php
@@ -0,0 +1,11 @@
+<!-- Write a PHP Program to convert Km to Mile (find formula and calculate) 10. Write a PHP Program to swap two numbers using third variable. -->
+<?php
+$num1 = 5; $num2 = 10;
+
+echo "num1 = $num1, num2 = $num2<br>";
+
+$temp = $num1;
+$num1 = $num2;
+$num2 = $temp;
+echo "Swapped: num1 = $num1, num2 = $num2";
+?>

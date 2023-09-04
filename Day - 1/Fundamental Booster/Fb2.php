92079bc35f71faafd6caa7ab7cb31bcf8d4c60cf
Deep Patel <118447327+deepbajud@users.noreply.github.com>
2023-09-04 13:40:23 +0530

    Create Fb2.php
    Write a PHP Program to find Triangle area (Write a PHP Program to find Triangle area (area = (1*h)/2))

---
+++ Day - 1/Fundamental Booster/Fb2.php
@@ -0,0 +1,10 @@
+<!-- Write a PHP Program to find Triangle area (area = (1*h)/2)
+we use this formula area = (1/2) · b · h -->
+<?php
+$base = 8;
+$height = 4;
+
+$area = (1/2) * $base * $height;
+
+echo "The area of the triangle is: " . $area;
+?>

784b866b4eb637493a4df8c0222fae43729cdeb5
Deep Patel <118447327+deepbajud@users.noreply.github.com>
2023-09-04 15:52:25 +0530

    Update Fb9.php
    Write a PHP Program to swap two numbers using third variable.

--- Day - 1/Fundamental Booster/Fb9.php
+++ Day - 1/Fundamental Booster/Fb9.php
@@ -1,4 +1,4 @@
-<!-- Write a PHP Program to convert Km to Mile (find formula and calculate) 10. Write a PHP Program to swap two numbers using third variable. -->
+<!-- Write a PHP Program to swap two numbers using third variable. -->
 <?php
 $num1 = 5; $num2 = 10;
 

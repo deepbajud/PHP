f297b7cb38227953b5253146c42e0e7817396565
Deep Patel <118447327+deepbajud@users.noreply.github.com>
2023-09-04 16:02:52 +0530

    Create Fb11.php
    Write a PHP Program to convert Km to Mile (find formula and calculate) 1km = 0.621371 miles

---
+++ Day - 1/Fundamental Booster/Fb11.php
@@ -0,0 +1,10 @@
+<!-- Write a PHP Program to convert Km to Mile (find formula and calculate) 1km = 0.621371 miles -->
+<?php
+$kilometers = 7;
+
+$formula = 0.621371;
+
+$miles = $kilometers * $formula;
+
+echo "$kilometers kilometers is equal to $miles miles.";
+?>
